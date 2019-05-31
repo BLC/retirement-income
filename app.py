5e16f0ffda2e1a1625fa58d98ea642df12706cb4
Hao Lu <luh@luh-macbook.local>
2019-05-30 23:25:01 -0500

    Add Target Calculation

--- app.py
+++ app.py
@@ -21,6 +21,19 @@ def process():
 
     return jsonify({'error':'Missing data!'})
 
+@app.route('/target',methods=['POST'])
+def target():
+    salary = float(request.form['salary'])
+    contribution = float(request.form['contribution'])/100
+    replacement_1 = float(request.form['replacement_1'])/100
+    replacement_2 = float(request.form['replacement_2'])/100
+
+    tax_rate = 0.2
+    take_home_income = salary * (1 - contribution) * (1 - tax_rate)
+
+    return jsonify({'target_0':take_home_income,'target_1':take_home_income * replacement_1,'target_2':take_home_income * replacement_2})
+
+
 
 if __name__ == "__main__":
     app.run(debug=False)
---
+++ static/js/target.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  $('#calc-take-home-income').on('click',function(event){
+
+    $.ajax({
+      data: {
+        salary: $('#salary').val(),
+        contribution: $('#contribution').val(),
+        replacement_1: $('#replacement-ratio-1').val(),
+        replacement_2: $('#replacement-ratio-2').val()
+,      },
+      type: 'POST',
+      url: '/target'
+
+    })
+    .done(function(data) {
+
+      if (data.error) {
+
+      }
+      else {
+
+        $('.take-home-income').text(data.target_0);
+        $('#non-dis-spend').text(data.target_1);
+        $('#dis-spend').text(data.target_2);
+      }
+
+    });
+  });
+});
+
+function calc_target_1(ratio) {
+  var target_income = $('#take-home-income').text();
+  $('#non-dis-spend').text(parseFloat(target_income) * parseFloat(ratio) / 100);
+};
+
+function calc_target_2(ratio) {
+  var target_income = $('#take-home-income').text();
+  $('#dis-spend').text(parseFloat(target_income) * parseFloat(ratio) / 100);
+};
