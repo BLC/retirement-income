6b56ddcf33db3493cc272ca23528821d852ab04f
Hao Lu <luh@luh-macbook.blc-hq.com>
2019-06-03 09:24:36 -0500

    Add data input

---
+++ python/calc_spend_down_age.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import numpy as np
+
+loc = os.path.join(os.getcwd(), '..','/data/mortality.csv')
+
+# mortality_table = pd.read_csv(loc)
+mortality_table = pd.read_csv('../data/mortality.csv')
